a1ac8909a23c7841e37336314a26d2ad32be9300
hanisafi <74617493+hanisafi@users.noreply.github.com>
2020-12-06 19:22:38 -0600

    Add files via upload

---
+++ stoptest.py
@@ -0,0 +1,80 @@
+import serial
+import time
+import RPi.GPIO as GPIO
+
+# for 1st Motor on ENA
+ENA = 33
+IN1 = 35
+IN2 = 37
+# for 2nd Motor on ENB
+ENB = 36
+IN3 = 38
+IN4 = 40
+
+# set pin numbers to the board's
+GPIO.setmode(GPIO.BOARD)
+
+# initialize EnA, In1 and In2
+GPIO.setup(ENA, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(ENB, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
+
+ser = serial.Serial("/dev/ttyTHS1", 115200)
+
+def getTFminiData():
+    while True:
+        #time.sleep(0.1)
+        count = ser.in_waiting
+        GPIO.output(ENA, GPIO.HIGH)
+        GPIO.output(ENB, GPIO.HIGH)
+        # Forward
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.HIGH)
+
+        if count > 8:
+            recv = ser.read(9)  
+            ser.reset_input_buffer()  
+            # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
+            # type(recv[0]), 'str' in python2, 'int' in python3 
+            
+            if recv[0] == 0x59 and recv[1] == 0x59:     #python3
+                distance = recv[2] + recv[3] * 256
+                strength = recv[4] + recv[5] * 256
+                print('(', distance, ',', strength, ')')
+                ser.reset_input_buffer()
+                
+            if recv[0] == 'Y' and recv[1] == 'Y':     #python2
+                lowD = int(recv[2].encode('hex'), 16)      
+                highD = int(recv[3].encode('hex'), 16)
+                lowS = int(recv[4].encode('hex'), 16)      
+                highS = int(recv[5].encode('hex'), 16)
+                distance = lowD + highD * 256
+                strength = lowS + highS * 256
+                print(distance, strength)
+                
+        if distance <= 50:
+            GPIO.output(ENA, GPIO.HIGH)
+            GPIO.output(ENB, GPIO.HIGH)
+            GPIO.cleanup()
+            break
+            
+            # you can also distinguish python2 and python3: 
+            #import sys
+            #sys.version[0] == '2'    #True, python2
+            #sys.version[0] == '3'    #True, python3
+
+
+if __name__ == '__main__':
+    try:
+        if ser.is_open == False:
+            ser.open()
+        getTFminiData()
+    except KeyboardInterrupt:   # Ctrl+C
+        if ser != None:
+            ser.close()
+            GPIO.cleanup()
