4f686804a8be9e2bd61b07110330c8c13a13b93b
solarexpress <74580109+solarexpress@users.noreply.github.com>
2020-12-08 17:38:12 -0600

    Add files via upload

---
+++ ustest.py
@@ -0,0 +1,119 @@
+# coding: utf-8
+import time
+import RPi.GPIO as GPIO
+
+# Use BOARD GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BOARD)
+
+# for 1st Motor on ENA
+ENA = 33
+IN1 = 35
+IN2 = 37
+# for 2nd Motor on ENB
+ENB = 36
+IN3 = 38
+IN4 = 40
+
+# initialize EnA, In1 and In2
+GPIO.setup(ENA, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(ENB, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
+
+# Stop
+GPIO.output(ENA, GPIO.HIGH)
+GPIO.output(ENB, GPIO.HIGH)
+
+# Forward
+GPIO.output(IN1, GPIO.HIGH)
+GPIO.output(IN2, GPIO.LOW)
+GPIO.output(IN3, GPIO.LOW)
+GPIO.output(IN4, GPIO.HIGH)
+
+
+# Define GPIO to use on Pi
+GPIO_TRIGECHO = 15
+
+print "Ultrasonic Measurement"
+
+# Set pins as output and input
+GPIO.setup(GPIO_TRIGECHO,GPIO.OUT)  # Initial state as output
+
+
+# Set trigger to False (Low)
+GPIO.output(GPIO_TRIGECHO, False)
+
+def measure():
+  # This function measures a distance
+  # Pulse the trigger/echo line to initiate a measurement
+    GPIO.output(GPIO_TRIGECHO, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGECHO, False)
+  #ensure start time is set in case of very quick return
+    start = time.time()
+
+  # set line to input to check for start of echo response
+    GPIO.setup(GPIO_TRIGECHO, GPIO.IN)
+    while GPIO.input(GPIO_TRIGECHO)==0:
+        start = time.time()
+
+  # Wait for end of echo response
+    while GPIO.input(GPIO_TRIGECHO)==1:
+        stop = time.time()
+  
+    GPIO.setup(GPIO_TRIGECHO, GPIO.OUT)
+    GPIO.output(GPIO_TRIGECHO, False)
+
+    elapsed = stop-start
+    distance = (elapsed * 34300)/2.0
+    time.sleep(0.1)
+    return distance
+
+try:
+
+  while True:
+      distance = measure()
+      print("  Distance : %.1f cm") % distance
+      time.sleep(.1)
+      if distance < 50:
+        print("Object detected!")
+        GPIO.output(ENA, GPIO.LOW)
+        GPIO.output(ENB, GPIO.LOW)
+        time.sleep(1)
+        print("Reversing...")
+        GPIO.output(ENA, GPIO.HIGH)
+        GPIO.output(ENB, GPIO.HIGH)
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.HIGH)
+        GPIO.output(IN3, GPIO.HIGH)
+        GPIO.output(IN4, GPIO.LOW)
+        time.sleep(.75)
+        print("Moving out of the way.")
+        GPIO.output(ENA, GPIO.HIGH)
+        GPIO.output(ENB, GPIO.HIGH)
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.HIGH)
+        GPIO.output(IN4, GPIO.LOW)
+        time.sleep(.25)
+
+      else:
+        print("Resuming forward motion...")
+        time.sleep(.5)
+        GPIO.output(ENA, GPIO.HIGH)
+        GPIO.output(ENB, GPIO.HIGH)
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.HIGH)
+
+
+except KeyboardInterrupt:
+  print("Stop")
+  GPIO.output(ENA, GPIO.LOW)
+  GPIO.output(ENB, GPIO.LOW)
+finally:
+  GPIO.cleanup()
